6de2e6b7dddeb0f773d1af732889bb5ed486b2e7
Zubair Majeed <zubairsundho@gmail.com>
2017-10-23 10:47:38 +0500

    FA14-BSE-061 Saqib ALI

---
+++ Iterator/src/ItemsRepository.java
@@ -0,0 +1,32 @@
+
+public class ItemsRepository implements Container {
+	public String names[] = {"Shirts", "Belts" ,"Trousers" , "Wallet"};
+
+	   @Override
+	   public Iterator getIterator() {
+	      return new NameIterator();
+	   }
+
+	   private class NameIterator implements Iterator {
+
+	      int index;
+
+	      @Override
+	      public boolean hasNext() {
+	      
+	         if(index < names.length){
+	            return true;
+	         }
+	         return false;
+	      }
+
+	      @Override
+	      public Object next() {
+	      
+	         if(this.hasNext()){
+	            return names[index++];
+	         }
+	         return null;
+	      }		
+	   }
+}
